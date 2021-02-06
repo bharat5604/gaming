f9f8427eee36c41abd30bc85a1ec4fbbe63487ab
iambharat <bhushansingh696@gmail.com>
2021-02-06 11:24:58 +0530

    update login and signup page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,20 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState } from "react";
+import { Navbar, Nav, Modal } from "react-bootstrap";
 import logo from "./Assets/img/logo.png";
 import { Link } from "react-router-dom";
+import facebook from "./Assets/img/facebook.png";
+import google from "./Assets/img/google.png";
+import user from "./Assets/img/user.png";
+import flag from "./Assets/img/flag.png";
+
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const [signup, setSignup] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleSignUp = () => setSignup(true);
+  const handleSignClose = () => setSignup(false);
   return (
     <header>
       <Navbar expand="lg">
@@ -37,10 +49,12 @@ const Header = () => {
                 <Link to="">cards</Link>
               </Nav.Link>
               <Nav.Link>
-                <button type="button">sign in</button>
+                <button type="button" onClick={handleShow}>
+                  sign in
+                </button>
               </Nav.Link>
               <Nav.Link>
-                <button className="signup" type="button">
+                <button className="signup" type="button" onClick={handleSignUp}>
                   sign up
                 </button>
               </Nav.Link>
@@ -51,6 +65,162 @@ const Header = () => {
           </Navbar.Collapse>
         </div>
       </Navbar>
+      <div className="loginMain">
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+          className="login"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Sign in</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form action="">
+              <div className="form-group">
+                <label htmlFor="username">UserName *</label>
+                <input
+                  type="text"
+                  placeholder="UserName"
+                  name="username"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password *</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <button type="submit">log in</button>
+              </div>
+              <div className="form-group forget">
+                <p>
+                  <input type="checkbox" /> remember me
+                </p>
+                <p>
+                  <Link to="">Forgot Your password?</Link>
+                </p>
+              </div>
+            </form>
+          </Modal.Body>
+          <div className="login__with">
+            <div className="heading">
+              <h3>
+                <span>or login with</span>
+              </h3>
+            </div>
+            <div className="apis">
+              <Link to="">
+                <img src={facebook} alt="" />
+              </Link>
+              <Link to="">
+                <img src={google} alt="" />
+              </Link>
+            </div>
+          </div>
+          <div className="signup">
+            <img src={user} alt="" />
+            <p>No Account Yet?</p>
+            <Link to="">Create An Account</Link>
+          </div>
+        </Modal>
+      </div>
+      <div className="signup">
+        <Modal
+          show={signup}
+          onHide={handleSignClose}
+          backdrop="static"
+          keyboard={false}
+          className="login"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Sign up</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form action="">
+              <div className="form-group">
+                <label htmlFor="username">UserName *</label>
+                <input
+                  type="text"
+                  placeholder="UserName"
+                  name="username"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email *</label>
+                <input type="email" placeholder="Email" name="email" required />
+              </div>
+              <div className="form-group number">
+                <label htmlFor="phone">Phone Number *</label>
+                <div className="mainFlag">
+                  <div className="flag">
+                    <img src={flag} alt="" />
+                    <select name="number" id="">
+                      <option value="91">+91</option>
+                    </select>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Phone"
+                    name="password"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password *</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="cpassword">Confirm Password *</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="cpassword"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <button type="submit">sign up</button>
+              </div>
+            </form>
+          </Modal.Body>
+          <div className="login__with">
+            <div className="heading">
+              <h3>
+                <span>or signup with</span>
+              </h3>
+            </div>
+            <div className="apis">
+              <Link to="">
+                <img src={facebook} alt="" />
+              </Link>
+              <Link to="">
+                <img src={google} alt="" />
+              </Link>
+            </div>
+          </div>
+          <div className="signup">
+            <img src={user} alt="" />
+            <p>already registerd with us?</p>
+            <Link to="" className="signin">
+              sign in
+            </Link>
+          </div>
+        </Modal>
+      </div>
     </header>
   );
 };
